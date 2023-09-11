ff2a4f7e3f556f9ef779f3c28a06015a2d7c5020
Niranjan Powade <niranjanpowadex@gmail.com>
2023-09-11 23:28:02 +0530

    LearningArrowFunctions

---
+++ W3Schools/Arrow-function/thisInArrow.js
@@ -0,0 +1,10 @@
+// Regular Function:
+hello = function () {
+  document.getElementById("demo").innerHTML += this;
+};
+
+// The window object calls the function:
+window.addEventListener("load", hello);
+
+// A button object calls the function:
+document.getElementById("btn").addEventListener("click", hello);
