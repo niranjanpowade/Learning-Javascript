8dedc6bb006b3965389920c21e05833a274e45ec
Niranjan Powade <niranjanpowadex@gmail.com>
2023-09-12 12:23:38 +0530

    Learning javascript in depth

---
+++ W3Schools/async-await.js
@@ -0,0 +1,9 @@
+async function fetchUserData() {
+  try {
+    let response = await fetch("https://jsonplaceholder.typicode.com/users");
+    let data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
---
+++ W3Schools/rest.js
@@ -0,0 +1,4 @@
+function myFunction(...args) {
+  console.log(args);
+}
+myFunction(1, 2, 3, 4, 5);
