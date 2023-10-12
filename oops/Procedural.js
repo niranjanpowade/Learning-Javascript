83efb34396350274f5a7d5ece6b3eb4b6744ec28
Niranjan Powade <niranjanpowadex@gmail.com>
2023-10-12 23:55:26 +0530

    Learning about procedural and functional programming

---
+++ oops/Procedural.js
@@ -0,0 +1,11 @@
+let basesalary = 3000;
+let overtime = 10;
+let time = 20;
+
+function getWage(basesalary, overtime, time) {
+  console.log(basesalary + overtime * time);
+}
+
+getWage(basesalary, overtime, time);
+
+//This is procedural implementation
